96854798d1a676c21246fb8646273cde77d2a9b2
kaioarakawa <kaioarakawa@hotmail.com>
2018-09-23 11:58:39 -0300

    arvore com desbalanceamento implementado

--- arvoreb.cpp
+++ arvoreb.cpp
@@ -5,33 +5,60 @@
 
 struct no{
 	int numero;
+	int dese;
 	struct no *esquerda;
 	struct no *direita; 
 	struct no *cima;
+	
 	//direita = próximo elemento e esquerda = elemento anteriror
 };
+void CalculoDoDesequilibrioEsquerda(struct no **lista);
+void CalculoDoDesequilibrioDireita(struct no **lista);
+void UltimoEsquerda(struct no **lista, int op);
+void UltimoDireita(struct no **lista, int op);
 void exibirPreOrdem(struct no **lista);
+int profundidade (struct no **lista);
+void EmOrdem(struct no **lista);
+void PosOrdem(struct no **lista);
 int po[9999];
 int k=0;
 int i=0;
 int tamanho=0;
-int profundidade=0;
+int profun=0;
 int valor;
 int type=0;
+struct no *lista= NULL, *pNavegar;
 
 int main(){
 	void inserir(struct no **lista);
 	void remover(struct no **lista);
-	struct no *lista, *pNavegar;
 	int opc=0, valor, cont;
-	lista = NULL;
 	for(;;){
-		if(profundidade<i){
-			profundidade=i;
+		
+		if(tamanho!=0){
+			while(lista->cima!=NULL){
+				lista=lista->cima;
+			}
+			if(lista->direita!=NULL && lista->direita->dese==NULL){
+				lista=lista->direita;
+				CalculoDoDesequilibrioDireita(&lista);
+			}else if(lista->esquerda!=NULL && lista->esquerda->dese==NULL){
+				lista=lista->esquerda;
+				CalculoDoDesequilibrioEsquerda(&lista);
+			}
+			if(lista->direita!=NULL && lista->esquerda!=NULL){
+				if(lista->direita->dese==101 && lista->esquerda->dese==101){
+					lista->dese=101;
+				}else{
+					lista->dese=lista->direita->dese-lista->esquerda->dese;
+				}
+			}
 		}
-		i=0;
-		printf("Tamanho: %i\n",profundidade);
-		printf("\n\nDigite a opçao desejada: \n1-Insert \n2-Remove \n3-Playlist \n4-Listar \n5-Sair\n\n");
+
+			
+		
+		
+		printf("\n\nDigite a opçao desejada: \n1-Insert \n2-Remove \n3-Playlist  \n4-Listar em pre ordem \n5-Listar em ordem \n6-Listar pos ordem  \n7-Profundidade  \n8-Sair\n\n");
 		scanf("%i",&opc);
 		
 		if(opc==1){
@@ -47,6 +74,9 @@ int main(){
 				for(;;){
 					
 					printf("\n\n\nAtual: %d\n\n",pNavegar->numero);
+					if(pNavegar->dese=!NULL){
+						printf("\nDesequilibrio: %d\n\n",pNavegar->dese);
+					}
 					printf("\n\nDigite a opçao desejada: \n1-Anterior \n2-Proximo \n3-Ir para cima \n4-Remover numero \n5-Sair\n\n");
 					scanf("%i",&opc);
 						if(opc==1){
@@ -93,8 +123,8 @@ int main(){
 				printf("Sem elementos na arvore");
 			}else{
 				for(;;){
-						
 						printf("\n\n\nAtual: %d\n\n",pNavegar->numero);
+						printf("\nDesequilibrio: %d\n\n",pNavegar->dese);
 						printf("\n\nDigite a opçao desejada: \n1-Esquerda \n2-Direita \n3-Ir para cima \n4-Sair\n\n");
 						scanf("%i",&opc);
 							if(opc==1){
@@ -127,15 +157,55 @@ int main(){
 			if(tamanho == 0){
 				printf("Lista Vazia");
 			}else{
+				while(lista->cima!=NULL){
+					lista=lista->cima;
+				}
 				exibirPreOrdem(&lista);
 				printf("\n\nArvore em pre-ordem\n\n");
 				for(i=0;i<k;i++){
 					printf("%i\t",po[i]);
 				}
+				k=0;
 				printf("\n\n\n");
 				system("pause");
 			}
 		}else if(opc==5){
+			if(tamanho == 0){
+				printf("Lista Vazia");
+			}else{
+				while(lista->cima!=NULL){
+					lista=lista->cima;
+				}
+				printf("\n\nArvore em ordem\n\n");
+				EmOrdem(&lista);
+				printf("\n\n\n");
+				system("pause");
+			}
+		}else if(opc==6){
+			if(tamanho == 0){
+				printf("Lista Vazia");
+			}else{
+				while(lista->cima!=NULL){
+					lista=lista->cima;
+				}
+				printf("\n\nArvore em pos ordem\n\n");
+				PosOrdem(&lista);
+				printf("\n\n\n");
+				system("pause");
+			}
+		}else if(opc==7){
+			if(tamanho==0){
+				printf("Lista Vazia");
+			}else{
+				while(lista->cima!=NULL){
+					lista=lista->cima;
+				}
+				profun=profundidade(&lista);
+				printf("Tamanho: %i\n",(profun-1));
+				system("pause");
+			}
+
+		}else if(opc==8){
 			printf("\n\n\n\nFechando a playlist, muito obrigado");
 			break;
 		}else{
@@ -181,6 +251,7 @@ void inserir(struct no **lista){
 			p->direita=NULL;
 			p->esquerda = NULL;
 			p->cima = NULL;
+			p->dese = NULL;
 			*lista=p;
 		}
 	}else{
@@ -211,32 +282,39 @@ void inserir(struct no **lista){
 						p->esquerda=NULL;
 						p->direita=NULL;
 						pNavegar->esquerda=p;
-						i++;
+						pNavegar->dese = NULL;
 						
 				}else{
 					while(p->numero<pNavegar->numero &&  pNavegar->esquerda!=NULL){
 						pNavegar=pNavegar->esquerda;
-						i++;
 					}
 					i++;
 					if(pNavegar->numero<p->numero){
 						while(p->numero>pNavegar->numero &&  pNavegar->direita!=NULL){
 							pNavegar=pNavegar->direita;
-							i++;
 						}
-						vt=pNavegar;
-						p->cima=vt;
-						p->esquerda=NULL;
-						p->direita=NULL;
-						pNavegar->direita=p;
-						
+						if(pNavegar->numero<p->numero){
+							vt=pNavegar;
+							p->cima=vt;
+							p->esquerda=NULL;
+							p->direita=NULL;
+							pNavegar->direita=p;
+							pNavegar->dese = NULL;	
+						}else{
+							vt=pNavegar;
+							p->cima=vt;
+							p->esquerda=NULL;
+							p->direita=NULL;
+							pNavegar->esquerda=p;
+							pNavegar->dese = NULL;
+						}
 					}else{
 						vt=pNavegar;
 						p->cima=vt;
 						p->esquerda=NULL;
 						p->direita=NULL;
 						pNavegar->esquerda=p;
-
+						pNavegar->dese = NULL;
 					}
 				}
 				
@@ -248,12 +326,10 @@ void inserir(struct no **lista){
 						p->esquerda=NULL;
 						p->direita=NULL;
 						pNavegar->direita=p;
-						i++;
-						
+						pNavegar->dese = NULL;
 				}else{
 					while(pNavegar->numero<p->numero && pNavegar->direita!=NULL){
 						pNavegar=pNavegar->direita;
-						i++;
 					}
 					i++;
 					if(pNavegar->numero<p->numero){
@@ -262,18 +338,27 @@ void inserir(struct no **lista){
 						p->esquerda=NULL;
 						p->direita=NULL;
 						pNavegar->direita=p;
-						
+						pNavegar->dese = NULL;
 						
 					}else{
 						while(p->numero<pNavegar->numero &&  pNavegar->esquerda!=NULL){
 							pNavegar=pNavegar->esquerda;
-							i++;
 						}
-						vt=pNavegar;
-						p->cima=vt;
-						p->esquerda=NULL;
-						p->direita=NULL;
-						pNavegar->esquerda=p;
+						if(pNavegar->numero<p->numero){
+							vt=pNavegar;
+							p->cima=vt;
+							p->esquerda=NULL;
+							p->direita=NULL;
+							pNavegar->direita=p;
+							pNavegar->dese = NULL;
+						}else{
+							vt=pNavegar;
+							p->cima=vt;
+							p->esquerda=NULL;
+							p->direita=NULL;
+							pNavegar->esquerda=p;
+							pNavegar->dese = NULL;	
+						}
 					}
 				}
 
@@ -408,6 +493,25 @@ void doisfilhos(struct no **lista){
 	
 }
 
+int profundidade (struct no **lista) {
+	struct no *pNavegar;
+	pNavegar=*lista;
+	int profune;
+	int profund;
+   if (pNavegar == NULL) 
+      return 0;
+   else {
+    	profune = profundidade (&pNavegar->esquerda);
+      	profund = profundidade (&pNavegar->direita);
+	    if (profune < profund){
+	    	return profund + 1;
+		} 
+	    else{
+	    	return profune + 1;
+		} 
+   }
+}
+
 void exibirPreOrdem(struct no **lista){
 	struct no *pNavegar;
 	pNavegar=*lista;
@@ -418,5 +522,181 @@ void exibirPreOrdem(struct no **lista){
         exibirPreOrdem(&pNavegar->direita);
     }
 }
+
+void EmOrdem(struct no **lista){
+	struct no *pNavegar;
+	pNavegar=*lista;
+    if(pNavegar != NULL){
+        EmOrdem(&pNavegar->esquerda);
+        printf("\n%i", pNavegar->numero);
+        EmOrdem(&pNavegar->direita);
+    }
+}
+
+void PosOrdem(struct no **lista){
+	struct no *pNavegar;
+	pNavegar=*lista;
+    if(pNavegar != NULL){
+        PosOrdem(&pNavegar->esquerda);
+        PosOrdem(&pNavegar->direita);
+        printf("\n%i", pNavegar->numero);
+    }
+}
+
+void CalculoDoDesequilibrioEsquerda(struct no **lista){
+	struct no *pNavegar;
+	pNavegar=*lista;
+	int dir;
+	int esquer;
+	if(pNavegar->cima==NULL && (pNavegar->esquerda->dese!=NULL || pNavegar->direita->dese!=NULL)){
+		main();
+	}else if(pNavegar->direita!=NULL && pNavegar->direita->dese==NULL){
+		*lista=pNavegar;
+		
+		UltimoDireita(lista,1);
+			
+	}else if(pNavegar->esquerda!=NULL && pNavegar->esquerda->dese==NULL ){
+		*lista=pNavegar;
+		UltimoEsquerda(lista,1);	
+	}else{
+		// 101 é igual a 0 no programa
+
+		if(pNavegar->esquerda==NULL && pNavegar->direita==NULL && pNavegar->dese==NULL){
+			pNavegar->dese=101;
+		}else if(pNavegar->esquerda!=NULL && pNavegar->direita!=NULL && pNavegar->dese==NULL){
+			if(pNavegar->direita->dese==101){
+				dir=1;
+			}else{
+				dir=pNavegar->direita->dese+1;
+			}
+			if(pNavegar->esquerda->dese==101){
+				esquer=-1;	
+			}else{
+				esquer=pNavegar->esquerda->dese-1;
+			}
+			if(dir-esquer==0){
+				pNavegar->dese=101;	
+			}else{
+				pNavegar->dese=dir-esquer;	
+			}
+			
+		}else if(pNavegar->esquerda!=NULL && pNavegar->direita==NULL && pNavegar->dese==NULL){
+			if(pNavegar->esquerda->dese==101){
+				esquer=-1;	
+			}else{
+				esquer=pNavegar->esquerda->dese-1;
+			}
+			printf("%i",esquer);
+			pNavegar->dese=esquer;
+		}else if(pNavegar->esquerda==NULL && pNavegar->direita!=NULL && pNavegar->dese==NULL){
+			if(pNavegar->direita->dese==101){
+				dir=1;
+			}else{
+				dir=pNavegar->direita->dese+1;
+			}
+			pNavegar->dese=dir;
+		}
+		pNavegar=pNavegar->cima;
+		*lista=pNavegar;	
+		CalculoDoDesequilibrioEsquerda(lista);
+		
+
+	}
+		
+}
+
+void CalculoDoDesequilibrioDireita(struct no **lista){
+	struct no *pNavegar;
+	pNavegar=*lista;
+	int dir;
+	int esquer;
+	if(pNavegar->cima==NULL && pNavegar->direita->dese!=NULL ){
+		main();
+	}else if(pNavegar->direita!=NULL && pNavegar->direita->dese==NULL ){
+		*lista=pNavegar;
+		UltimoDireita(lista,2);	
+	}else if(pNavegar->esquerda!=NULL && pNavegar->esquerda->dese==NULL ){
+		*lista=pNavegar;
+		UltimoEsquerda(lista,2);	
+	}else{
+		// 101 é igual a 0 no programa
+
+		if(pNavegar->esquerda==NULL && pNavegar->direita==NULL && pNavegar->dese==NULL){
+			pNavegar->dese=101;
+		}else if(pNavegar->esquerda!=NULL && pNavegar->direita!=NULL && pNavegar->dese==NULL){
+			if(pNavegar->direita->dese==101){
+				dir=1;
+			}else{
+				dir=pNavegar->direita->dese+1;
+			}
+			if(pNavegar->esquerda->dese==101){
+				esquer=-1;	
+			}else{
+				esquer=pNavegar->esquerda->dese-1;
+			}
+			if(dir-esquer==0){
+				pNavegar->dese=101;	
+			}else{
+				pNavegar->dese=dir-esquer;	
+			}
+			
+		}else if(pNavegar->esquerda!=NULL && pNavegar->direita==NULL && pNavegar->dese==NULL){
+			if(pNavegar->esquerda->dese==101){
+				esquer=-1;	
+			}else{
+				esquer=pNavegar->esquerda->dese-1;
+			}
+			printf("%i",esquer);
+			pNavegar->dese=esquer;
+		}else if(pNavegar->esquerda==NULL && pNavegar->direita!=NULL && pNavegar->dese==NULL){
+			if(pNavegar->direita->dese==101){
+				dir=1;
+			}else{
+				dir=pNavegar->direita->dese+1;
+			}
+			pNavegar->dese=dir;
+		}
+		pNavegar=pNavegar->cima;
+		*lista=pNavegar;	
+		CalculoDoDesequilibrioDireita(lista);
+		
+
+	}
+		
+}
+
+void UltimoDireita(struct no **lista, int op){
+	struct no *pNavegar;
+	pNavegar=*lista;
+	while(pNavegar->direita!=NULL && pNavegar->dese==NULL){
+		pNavegar=pNavegar->direita;
+	}
+	*lista=pNavegar;
+	if(op==1){
+		CalculoDoDesequilibrioEsquerda(lista);
+	}else{
+		CalculoDoDesequilibrioDireita(lista);
+	}	
+	
+}
+
+void UltimoEsquerda(struct no **lista, int op){
+	struct no *pNavegar;
+	pNavegar=*lista;
+
+	while(pNavegar->esquerda!=NULL && pNavegar->dese==NULL){
+		pNavegar=pNavegar->esquerda;
+	}
+	
+	*lista=pNavegar;
+
+	
+	if(op==1){
+		CalculoDoDesequilibrioEsquerda(lista);
+	}else{
+		CalculoDoDesequilibrioDireita(lista);
+	}		
+}
+
 
 
